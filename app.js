74a49450821ac3f0f8909befb05d017189bfafd2
Ivory Parker <parkeri4@southernct.edu>
2025-09-22 17:40:54 +0000

    hw

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const http = require('http');
 const url = require('url');
 
@@ -22,6 +23,9 @@ let serverObj =  http.createServer(function(req,res){
 		case "/cancel":
 			cancel(urlObj.query,res);
 			break;
+		case "/check":
+			check(urlObj.query,res);
+			break;
 		default:
 			error(res,404,"pathname unknown");
 
@@ -31,6 +35,16 @@ let serverObj =  http.createServer(function(req,res){
 function schedule(qObj,res) {
 	if (availableTimes[qObj.day].some(time => time == qObj.time))
 	{
+		//removes the time from the array above (filter, test elements in the array)
+		availableTimes[qObj.day] = availableTimes[qObj.day].filter(time => time !== qObj.time);
+		
+		//adds to the appointments array (push, to the end of the array)
+		appointments.push({
+           	   name: qObj.name,
+		   day: qObj.day,
+		   time: qObj.time
+	        });
+
 		res.writeHead(200,{'content-type':'text/plain'});
 		res.write("scheduled");
 		res.end();
@@ -41,8 +55,54 @@ function schedule(qObj,res) {
  
 }
 
-function cancel(qObj) {
+
+
+function cancel(qObj, res) {
+
+    //finds the index of the appointment
+    const index = appointments.findIndex(placeholder =>
+        placeholder.name === qObj.name && placeholder.day === qObj.day && placeholder.time === qObj.time
+    );
+
+    if (index !== -1) {
+        //removes the appointment
+        appointments.splice(index, 1);
+
+        //add the time back to availableTimes
+        if (!availableTimes[qObj.day].some(time => time === qObj.time)) {
+            availableTimes[qObj.day].push(qObj.time);
+        }
+
+        res.writeHead(200, { 'content-type': 'text/plain' });
+        res.write("Appointment has been canceled");
+        res.end();
+    } else {
+        error(res, 404, "Appointment not found");
+    }
 }
+
+function check(qObj, res){
+    
+    //Makes sure the day has available times, if not return
+    if (!availableTimes[qObj.day]) {
+        res.writeHead(400, { 'content-type': 'text/plain' });
+        res.write("No appointment times are available on ${qObj.day}");
+        res.end();
+        return;
+    }
+
+    //Day is available, the following are the outputs
+    if (availableTimes[qObj.day].some(time => time === qObj.time)) {
+        res.writeHead(200, { 'content-type': 'text/plain' });
+        res.write(`The appointment at  ${qObj.time} on ${qObj.day} is available`);
+    } else {
+        res.writeHead(200, { 'content-type': 'text/plain' });
+        res.write(`The appointment at  ${qObj.time} on ${qObj.day} is not available`);
+    }
+    res.end();
+
+}
+
 
 function error(response,status,message) {
 
